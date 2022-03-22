c9352f252c8af1a91eb37f6bf8044426d110f2e1
Julien <gerard.julien2000@orange.fr>
2022-03-22 15:53:33 +0100

    Widgets

--- src/Widget.js
+++ src/Widget.js
@@ -1,10 +1,25 @@
 import React from "react";
 import "./Widgets.css";
+import { TwitterTimelineEmbed, TwitterTweetEmbed } from "react-twitter-embed";
+import SearchIcon from "@mui/icons-material/Search";
 
 function Widgets() {
   return (
     <div className="widgets">
-      <h2>Widgets</h2>
+      <div className="widgets_input">
+        <SearchIcon className="widgets_searchIcon" />
+        <input placeholder="Search Twitter" type="text" />
+      </div>
+      <div className="widgets_widgetContainer">
+        <h2>What's happening</h2>
+        <TwitterTweetEmbed tweetId={"1506269745795973127"} />
+
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="PSG_inside"
+          options={{ height: 400 }}
+        />
+      </div>
     </div>
   );
 }
